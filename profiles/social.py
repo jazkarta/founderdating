e5cfe1b6ece927ddd5086c08b36491de5f63506d
Rocky Burt <rocky@serverzen.com>
2011-10-25 15:45:51 -0230

    Some changes to support linking a user record

--- profiles/social.py
+++ profiles/social.py
@@ -17,16 +17,17 @@ def linkedin_extra_values(sender, user, response, details, **kwargs):
     except FdProfile.DoesNotExist:
         pass
 
-    fd_profile = FdProfile(linkedin_url=linkedin_url)
+    fd_profile = FdProfile(linkedin_url=linkedin_url,
+                           user=user)
+    fd_profile.save()
     ll_profile = LinkedinProfile(
-        fd_profile=fd_profile,
+        fd_profile_id=fd_profile.id,
         profile_raw=response.get('summary', u''),
         profile_picture=response.get('picture-url', u''),
         profile_location=response.get('location', u''),
         profile_industry=response.get('industry', u''),
         #connections_raw=response['connections'],
         )
-    fd_profile.save()
     ll_profile.save()
     return True
 
