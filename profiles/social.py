993c317ee01a90174ca64e61209694338277fa38
Rocky Burt <rocky@serverzen.com>
2011-10-27 07:38:23 -0230

    Added comment about field mapping

--- profiles/social.py
+++ profiles/social.py
@@ -5,6 +5,22 @@ from social_auth.backends.contrib.linkedin import LinkedinBackend
 from .models import LinkedinProfile, FdProfile
 
 
+# last field mappings as provided by Jessica
+# - Name - use LinkedIn
+# - City - Use LinkedIN (new)
+# * Note: Event - this should be different than city in that it will
+#         be the event date
+# Previous Experience: this is where we want to pull in both previous
+# and current work experience and education. If there is text filled
+# in on LInkedIn about each position, we should pull that in as well.
+# - websites - - use linkedin
+# - Twitter - use linkedin
+# - Connections In Common
+# - this is a nice to have, but if it's easy or part of the API we
+#   can show the # of connections the viewer of the profile has in
+#   common with the profile owner
+
+
 def linkedin_extra_values(sender, user, response, details, **kwargs):
     """
     """
